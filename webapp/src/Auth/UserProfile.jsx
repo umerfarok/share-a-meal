eba95acce20f7b855733f5f17e4ccc56e0461467
umerfarok <umerfarooq.dev@gmail.com>
2024-05-03 19:22:06 +0500

    chore: Update UserProfile component to redirect to login if user is not authenticated

--- webapp/src/Auth/UserProfile.jsx
+++ webapp/src/Auth/UserProfile.jsx
@@ -1,16 +1,22 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { AuthContext } from "./AuthContext"
 
 export default function UserProfile() {
   const { user, signOut } = useContext(AuthContext)
+
+  useEffect(() => {
+    if (!user) {
+      window.location.href = "/login"
+    }
+  }, [user])
 
   return (
     <div>
       {user && (
         <div>
           <h2>User Profile</h2>
-          <p>Username: {user.username}</p>
-          <p>Email: {user.email}</p>
+          <p>Username:{user.idToken.payload["cognito:username"]}</p>
+          <p>Email: {user.idToken.payload.email}</p>
           {/* Display any other user data here */}
         </div>
       )}
